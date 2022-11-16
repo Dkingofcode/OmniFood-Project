e08b5a55a1343ff4842d2638f6f1ddf5236c1e8c
Dkingofcode <doladepo128@gmail.com>
2022-11-16 14:55:14 +0000

    Finished mobile layout and responsiveness added js file

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Hello world!");
+
+const myName = "Jonas Brandt";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+
+// MOBILE NAVIGATION
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
