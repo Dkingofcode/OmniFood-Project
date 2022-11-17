0625f2a7baad20f2d38c214c08dfa86603389bb6
Dkingofcode <doladepo128@gmail.com>
2022-11-17 19:37:53 +0000

    implemented mobile screen navigation with javascript

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ console.log(h1);
 
 
 // MOBILE NAVIGATION
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
+btnNav.addEventListener('click', function(){
+    headerEl.classList.toggle('nav-open');
+});
 
+// Smooth scrolling animation
 
+const allLinks = document.querySelectorAll('a:link');
 
-
+allLinks.forEach(function(link){
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+        const href = link.getAttribute('href');
+        console.log(href);
+    });
+});
 
 
 
