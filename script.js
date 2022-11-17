7e07f6779671c0c318634d44b77d2ccce678f0d3
Dkingofcode <doladepo128@gmail.com>
2022-11-17 21:19:09 +0000

    Finished with sticky header and smooth-scroll

--- script.js
+++ script.js
@@ -23,12 +23,54 @@ allLinks.forEach(function(link){
         e.preventDefault();
         const href = link.getAttribute('href');
         console.log(href);
+
+        // Scroll back to top
+        if(href === "#")
+           window.scrollTo({
+              top: 0,
+              behaviour: "smooth",
+           });
+
+
+           // Scroll to other links
+           if (href !== "#" && href.startsWith('#')){
+             const sectionEl = document.querySelector(href);
+             sectionEl.scrollIntoView({ behaviour: "smooth" });
+           }
+
+           // Close mobile navigation
+           if(link.classList.contains("main-nav-link"))
+           headerEl.classList.toggle("nav-open");
     });
 });
+const sectionHero = document.querySelector(".section-hero") 
 
+const obs = new IntersectionObserver(
+    function(entries) {
+       const ent = entries[0];
+       console.log(ent);
 
+       if(ent.intersecting === false) {
+         document.body.classList.add("sticky");  
+       }
+    }, 
+    {
+        // In the viewport
+        root: null,
+        threshold: 0,
+    })
+obs.observe(sectionHero);
 
+// Fixing flexbox gap property missing in some Safari versions
+function  checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
 
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+}
 
 
 
